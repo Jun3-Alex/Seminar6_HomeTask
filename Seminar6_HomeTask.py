7826b492680cfcd1305910e452370757441b4bc5
Jun3-Alex <130096011+Jun3-Alex@users.noreply.github.com>
2023-09-04 01:00:31 +0500

    dicide second task

--- Seminar6_HomeTask.py
+++ Seminar6_HomeTask.py
@@ -18,4 +18,33 @@ def arithm_prog(a1, d, n):
 print(arithm_prog(a1, d, n))
 '''
 
+# Задача 32: Определить индексы элементов массива (списка),
+# значения которых принадлежат заданному диапазону (т.е. не
+# меньше заданного минимума и не больше заданного
+# максимума)
+'''
+def generate_list(n):
+    list_ = []
+    for i in range(n):
+        elements = int(input(f'Введите элемент массива: '))
+        list_.append(elements)
+    return list_
 
+def index_of_range(list_1, minimum, maximum):
+    if minimum < maximum:
+        index_list = []
+        for i in range(len(list_1)):
+            if minimum <= list_1[i] <= maximum:
+                index_list.append(i)
+        return index_list
+    else:
+        print(f'Введите корректные границы диапозона.')
+
+n = int(input(f'Введите количество элементов массива: '))
+list_1 = generate_list(n)
+
+minimum = int(input(f'Задайте минимум диапозона: '))
+maximum = int(input(f'Задайте максимум диапозона: '))
+
+print(f'Индексы элементов входящих в диапозон:{index_of_range(list_1, minimum, maximum)}')
+'''
